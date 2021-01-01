4a2e73b1d82ebf30bf61f541b36e04dddec87815
Brais Domínguez Álvarez <braisda@gmail.com>
2021-01-01 17:51:46 +0100

    Correccion servicio actividad
    Correccion servicio actividad

--- src/main/java/es/uvigo/esei/mei/tiburones/servicios/ActividadServiceImpl.java
+++ src/main/java/es/uvigo/esei/mei/tiburones/servicios/ActividadServiceImpl.java
@@ -13,43 +13,43 @@ import es.uvigo.esei.mei.tiburones.entidades.Investigador;
 @Service
 public class ActividadServiceImpl implements ActividadService {
 	@Autowired
-	ActividadDAO ataqueDAO;
+	ActividadDAO actividadDAO;
 
 
 	@Override
 	@Transactional
 	public Actividad crear(Actividad ataque) {
-		return ataqueDAO.save(ataque);
+		return actividadDAO.save(ataque);
 	}
 
 	@Override
 	@Transactional
 	public Actividad modificar(Actividad ataque) {
-		return ataqueDAO.save(ataque);
+		return actividadDAO.save(ataque);
 	}
 
 	@Override
 	@Transactional
 	public void eliminar(Actividad ataque) {
-		ataqueDAO.delete(ataque);
+		actividadDAO.delete(ataque);
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public Actividad buscarPorId(Long id) {
-		return ataqueDAO.getOne(id);
+		return actividadDAO.getOne(id);
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public List<Actividad> buscarTodos() {
-		return ataqueDAO.findAll();
+		return actividadDAO.findAll();
 	}
 
 	@Override
 	@Transactional(readOnly = true)
 	public List<Actividad> buscarPorTipo(String patron) {
-		return ataqueDAO.findByTipoContaining(patron);
+		return actividadDAO.findByTipoContaining(patron);
 	}
 
 
